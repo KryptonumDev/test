f0a39145fec9343e9855d18bc376ea10f7d3f814
kryptonumstudio <kryptonumstudio@gmail.com>
2023-01-06 14:11:28 +0100

    render way

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,22 +2,15 @@ const fs = require('fs');
 const { resolve } = require('path');
 const { get } = require('https');
 
-// exports.createPages = async ({
-//     graphql,
-//     actions: { createPage },
-// }) => {
-//     const { data: { wpPage } } = await graphql(`
-//                 query {
-//                     wpPage(homepage: {heroHome: {title: {ne: null}}}) {
-//                         id
-//                     }
-//                 }
-//           `);
+exports.createPages = async ({
+    graphql,
+    actions: { createPage },
+}) => {
 
-//     createPage({
-//         path: '/',
-//         component: resolve('src/templates/homepage.js'),
-//         context: {},
-//         ownerNodeId: wpPage.id,
-//     });
-// }
+    createPage({
+        path: '/',
+        component: resolve('src/templates/homepage.js'),
+        context: {},
+        ownerNodeId: 123456,
+    });
+}
---
+++ src/templates/homepage.js
@@ -0,0 +1,27 @@
+import { graphql } from "gatsby"
+import * as React from "react"
+
+const IndexPage = ({ data: { wpPage } }) => {
+    return (
+        <main>
+            {wpPage.content}
+            <br />
+            {wpPage.homepage.heroHome.title}
+        </main>
+    )
+}
+
+export const query = graphql`
+  query MyQuery {
+    wpPage(homepage: {heroHome: {title: {ne: null}}}) {
+        content
+      homepage {
+        heroHome {
+          title
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
